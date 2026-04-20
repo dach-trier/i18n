5dd1f67c5c9c4317a716daf612d1cf31f10b6a8c
Volodymyr But <git@hiegz.com>
2026-04-20 12:27:30 +0200

    render translations as html templates

--- html/funcs.go
+++ html/funcs.go
@@ -1,18 +1,32 @@
 package i18n_html
 
 import (
+	"bytes"
 	"fmt"
-	"golang.org/x/text/language"
 	"html/template"
+
+	"golang.org/x/text/language"
 
 	"github.com/dach-trier/i18n"
 )
 
 func FuncMap(bundle i18n.Bundle) template.FuncMap {
 	return template.FuncMap{
-		"t": func(message string, lang language.Tag, args ...any) (template.HTML, error) {
+		"t": func(message string, lang language.Tag, data any) (template.HTML, error) {
 			if translation, ok := i18n.Translate(message, lang, bundle); ok {
-				return template.HTML(translation), nil
+				var html bytes.Buffer
+				var tmpl *template.Template
+				var err error
+
+				if tmpl, err = template.New("").Parse(translation); err != nil {
+					return "", err
+				}
+
+				if err = tmpl.Execute(&html, data); err != nil {
+					return "", err
+				}
+
+				return template.HTML(html.String()), nil
 			}
 
 			return "", fmt.Errorf("missing translation for %q in language %q", message, lang)
